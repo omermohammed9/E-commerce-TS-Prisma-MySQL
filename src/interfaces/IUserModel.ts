9d740337a7c12f50edde9d4f29be474bfa1f1730
scorpiraq <96728173+scorpiraq@users.noreply.github.com>
2024-02-19 22:07:51 +0300

    Interface for User Model

--- src/interfaces/IUserModel.ts
+++ src/interfaces/IUserModel.ts
@@ -3,11 +3,13 @@ import {User as PrismaUser} from "@prisma/client";
 
 export interface IUserModel {
     createUser(userDetails: CreateUserDTO): Promise<PrismaUser>;
-    getUserById(userId: number): Promise<PrismaUser | null>;
-    //getUserByEmail(email: string): Promise<PrismaUser | null>;
     getAllUsers(): Promise<PrismaUser[]>;
+    getUserById(userId: number): Promise<PrismaUser | null>;
+    getUserByEmail(email: string): Promise<PrismaUser | null>;
     updateUser(userId: number, updateDetails: UpdateUserDTO): Promise<PrismaUser>;
     deleteUser(userId: number): Promise<void>;
+    changePassword(userId: number,  newPassword: string): Promise<void>;
+    //checkUserExists(email: string): Promise<boolean>;
+    //login(email: string, password: string): Promise<{ user: PrismaUser; token: string }>;
     //verifyUserPassword(email: string, password: string): Promise<boolean>;
-    // Additional methods for profile management, permissions, etc.
 }
