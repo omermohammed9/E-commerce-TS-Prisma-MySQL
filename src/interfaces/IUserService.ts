11176ebabd73ca149098557fbaf75208c697799e
scorpiraq <96728173+scorpiraq@users.noreply.github.com>
2024-02-19 22:08:22 +0300

    Interface for User Service

--- src/interfaces/IUserService.ts
+++ src/interfaces/IUserService.ts
@@ -1,13 +1,20 @@
-import {CreateUserDTO, UpdateUserDTO} from "../types/user.types";
-import {userResponse} from "../types/userResponse";
+import {CreateUserDTO, UpdateUserDTO, userResponse} from "../types/user.types";
+
+import {User} from "@prisma/client";
+import {UpdateUserAttributes} from "../utils/updateUserAttributes";
 
 export interface IUserService {
     createUser(userDetails: CreateUserDTO): Promise<userResponse>;
     getUserById(userId: number): Promise<userResponse | null>;
-    //getUserByEmail(email: string): Promise<userResponse>;
+    getUserByEmail(email: string): Promise<userResponse | null>;
     getAllUsers(): Promise<userResponse[]>;
-    updateUser(userId: number, updateDetails: UpdateUserDTO): Promise<userResponse>;
+    updateUser(userId: number, updateDetails: UpdateUserDTO): Promise<UpdateUserAttributes>;
     deleteUser(userId: number): Promise<{ message: string; status: 200 | 404  }>;
+
+    login(email: string, password: string): Promise<{ user: User; token: string }>;
+    changePassword(userId: number, oldPassword: string, newPassword: string): Promise<{ message: string }>;
+
+    //checkUserExists(email: string): Promise<boolean
     //verifyUserPassword(email: string, password: string): Promise<boolean>;
-    //Additional methods for profile management, permissions, etc.
+
 }
