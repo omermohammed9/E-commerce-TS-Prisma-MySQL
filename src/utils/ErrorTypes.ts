21e9020088b0115a70fac7990013a2d21563d52c
scorpiraq <96728173+scorpiraq@users.noreply.github.com>
2024-02-21 23:28:51 +0300

    adding ErrorTypes.ts types

--- src/utils/ErrorTypes.ts
+++ src/utils/ErrorTypes.ts
@@ -1,13 +1,20 @@
-export class UserNotFoundError  extends Error {
+ export class UserNotFoundError  extends Error {
     constructor(message?: string) {
-        super(message || "User not found.");
-        this.name = "ErrorTypes";
+        super(message || `User not found.`);
+        this.name = `ErrorTypes`;
+    }
+}
+
+export class OrderNotFoundError extends Error {
+    constructor(message?: string) {
+        super(message ||   `Order not found.`);
+        this.name = `OrderNotFoundError`;
     }
 }
 
 export class InvalidPasswordError extends Error {
     constructor(message?: string) {
-        super(message || "Invalid password.");
-        this.name = "InvalidPasswordError";
+        super(message || `Invalid password.`);
+        this.name = `InvalidPasswordError`;
     }
 }
