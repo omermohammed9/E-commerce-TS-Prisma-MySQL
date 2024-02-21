31f1accc4d13e168b2b02f01f7313feb9c4c70ec
scorpiraq <96728173+scorpiraq@users.noreply.github.com>
2024-02-21 23:29:12 +0300

    inversify config

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,14 +1,21 @@
 import { Container } from 'inversify';
 import { IUserService } from './interfaces/IUserService';
 import { UserService } from './services/UserService';
-import "reflect-metadata";
 import {UserModel} from "./models/user.model";
 import {TYPES} from "./types/types";
 import {IUserModel} from "./interfaces/IUserModel";
+import {UserController} from "./controller/UserController";
+import {OrderModel} from "./models/order.model";
+import {IOrderModel} from "./interfaces/IOrderModel";
+import {OrderService} from "./services/OrderService";
+import {IOrderService} from "./interfaces/IOrderService";
+import {OrderController} from "./controller/OrderController";
 
-const container = new Container();
-container.bind<IUserService>(TYPES.IUserService).to(UserService);
+export const container = new Container();
+
 container.bind<IUserModel>(TYPES.IUserModel).to(UserModel);
-
-
-export { container };
+container.bind<IUserService>(TYPES.IUserService).to(UserService);
+container.bind<UserController>(TYPES.UserController).to(UserController);
+container.bind<IOrderModel>(TYPES.IOrderModel).to(OrderModel);
+container.bind<IOrderService>(TYPES.IOrderService).to(OrderService);
+container.bind<OrderController>(TYPES.OrderController).to(OrderController);
